f603737585ebad302a6310daa2452084198638a1
maximart <maxime.martinez96@hotmail.fr>
2025-06-19 10:23:03 +0200

    feat(mandatory): add the expected files

---
+++ cpp01/ex01/src/Zombie.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maximart <maximart@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 16:20:40 by maximart          #+#    #+#             */
+/*   Updated: 2025/06/16 16:22:58 by maximart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+Zombie::Zombie()
+{
+}
+
+Zombie::Zombie(std::string name)
+{
+	_name = name;
+}
+
+Zombie::~Zombie()
+{
+}
+
+void Zombie::announce()
+{
+	std::cout << _name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
