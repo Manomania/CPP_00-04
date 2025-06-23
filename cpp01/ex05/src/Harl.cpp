582074487efa50c58df639a7eebf1fa7885809eb
maximart <maxime.martinez96@hotmail.fr>
2025-06-23 10:22:24 +0200

    feat(mandatory): subject is understood and done

--- cpp01/ex05/src/Harl.cpp
+++ cpp01/ex05/src/Harl.cpp
@@ -1,6 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maximart <maximart@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 09:01:33 by maximart          #+#    #+#             */
+/*   Updated: 2025/06/23 09:03:19 by maximart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
-#include <iostream>
+
 Harl::Harl() {
 }
+
 Harl::~Harl() {
+}
+
+void	Harl::_debug()
+{
+	std::cout << "DEBUG" << std::endl;
+}
+
+void	Harl::_info()
+{
+	std::cout << "INFO" << std::endl;
+}
+
+void	Harl::_warning()
+{
+	std::cout << "WARNING" << std::endl;
+}
+
+void	Harl::_error()
+{
+	std::cout << "ERROR" << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	std::string stringLevel[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*function[4])() = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
+	for (int i = 0; i < 4; i++)
+	{
+		if (stringLevel[i] == level)
+		{
+			(this->*function[i])();
+			return ;
+		}
+	}
+	std::cout << "Cannot resolve your problem" << std::endl;
 }
--- cpp01/ex05/src/main.cpp
+++ cpp01/ex05/src/main.cpp
@@ -1,10 +1,28 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maximart <maximart@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/23 09:01:33 by maximart          #+#    #+#             */
+/*   Updated: 2025/06/23 09:03:29 by maximart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
-int main() {
-    std::cout << "Hello from ex05 project!" << std::endl;
-	Harl Harl_instance;
-	std::cout << "Using Harl" << std::endl;
+int main()
+{
+	Harl level;
 
-    return 0;
+	level.complain("DEBUG");
+	level.complain("DEBG");
+	level.complain("INFO");
+	level.complain("INO");
+	level.complain("WARNING");
+	level.complain("WARNNG");
+	level.complain("ERROR");
+	level.complain("ERRR");
+	return (0);
 }
