e35f49f06ebd592654ecd4daa8043b63dac088fc
maximart <maxime.martinez96@hotmail.fr>
2025-07-14 13:37:57 +0200

    feat(class): add the necessary construct and public methods

--- cpp04/ex00/include/Animal.hpp
+++ cpp04/ex00/include/Animal.hpp
@@ -2,8 +2,17 @@
 #include <iostream>
 
 class Animal {
-	private:
+	protected:
+		std::string _type;
 	public:
+	// Constructor
 		Animal();
+		Animal(const std::string& name);
+		Animal(const Animal& copy);
+	// Overloaded Operator
+		Animal& operator=(const Animal& copy);
+	// Destructor
 		~Animal();
+	// Public Methods
+		void	makeSound();
 };
