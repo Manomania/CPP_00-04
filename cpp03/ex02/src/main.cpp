cb3d2ab16ce96a88574b1521a7b8e7ba105f54b1
maximart <maxime.martinez96@hotmail.fr>
2025-07-14 11:00:08 +0200

    feat(mandatory): add more tests

--- cpp03/ex02/src/main.cpp
+++ cpp03/ex02/src/main.cpp
@@ -3,30 +3,59 @@
 #include "FragTrap.hpp"
 
 int main() {
-	ClapTrap ClapTrap1("Zebi");
-	ClapTrap ClapTrap2;
-	ScavTrap ScavTrap1("Senior");
-	ScavTrap ScavTrap2;
-	FragTrap FragTrap1("Mamamia");
-	FragTrap FragTrap2;
+	std::cout << "\n===CONSTRUCTOR DEFAULT CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap1;
+	ClapTrap1.attack("model_1");
+	ClapTrap1.takeDamage(200);
+	ClapTrap1.beRepaired(1);
 
-	ClapTrap1.attack("robot");
-	ClapTrap1.takeDamage(10);
-	ClapTrap1.beRepaired(10);
-	ClapTrap1.attack("robot");
+	std::cout << "\n===CONSTRUCTOR ASSIGNMENT CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap2("ClapTrap_Assignment");
+	ClapTrap2.attack("model_2");
 	ClapTrap2.takeDamage(9);
-	ClapTrap2.beRepaired(11);
-	for (int i = 0; i < 11; i++)
-		ClapTrap2.attack("other robot");
-	ScavTrap1.attack("robot");
-	ScavTrap1.takeDamage(1000);
-	ScavTrap1.beRepaired(10);
-	ScavTrap1.attack("robot");
-	ScavTrap2.takeDamage(10);
-	ScavTrap2.beRepaired(11);
+	ClapTrap2.beRepaired(10);
+
+	std::cout << "\n===CONSTRUCTOR COPY CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap3(ClapTrap1);
+	ClapTrap3.attack("model_3");
+	ClapTrap3.takeDamage(9);
+	ClapTrap3.beRepaired(10);
+
+	std::cout << "\n===OVERLOADED OPERATOR= CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap4("Temp");
+	ClapTrap4 = ClapTrap1;
+	ClapTrap4.attack("model_4");
+	ClapTrap4.beRepaired(10);
+
+	std::cout << "\n===CONSTRUCTOR DEFAULT SCAVTRAP===\n" << std::endl;
+	ScavTrap ScavTrap1;
+	ScavTrap1.attack("model_1");
+	ScavTrap1.takeDamage(200);
+	ScavTrap1.beRepaired(1);
+	ScavTrap1.guardGate();
+
+	std::cout << "\n===CONSTRUCTOR ASSIGNMENT SCAVTRAP===\n" << std::endl;
+	ScavTrap ScavTrap2("ScavTrap_Assignment");
+	ScavTrap2.attack("model_2");
+	ScavTrap2.takeDamage(9);
+	ScavTrap2.beRepaired(10);
 	ScavTrap2.guardGate();
-	for (int i = 0; i < 11; i++)
-		ScavTrap2.attack("other robot");
+
+	std::cout << "\n===CONSTRUCTOR COPY SCAVTRAP===\n" << std::endl;
+	ScavTrap ScavTrap3(ScavTrap1);
+	ScavTrap3.attack("model_3");
+	ScavTrap3.takeDamage(9);
+	ScavTrap3.beRepaired(10);
+	ScavTrap3.guardGate();
+
+	std::cout << "\n===OVERLOADED OPERATOR= SCAVTRAP===\n" << std::endl;
+	ScavTrap ScavTrap4("Temp");
+	ScavTrap4 = ScavTrap1;
+	ScavTrap4.attack("model_4");
+	ScavTrap4.beRepaired(10);
+	ScavTrap4.guardGate();
+
+	std::cout << "\n===DESTRUCTORS===\n" << std::endl;
 	FragTrap1.attack("robot");
 	FragTrap1.takeDamage(1000);
 	FragTrap1.beRepaired(10);
