b1f50e51b97394a3cf16049ed848dda4505d8230
maximart <maxime.martinez96@hotmail.fr>
2025-07-14 08:11:45 +0200

    feat(mandatory): add more tests

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -1,29 +1,28 @@
 #include "ClapTrap.hpp"
 
 int main() {
-	ClapTrap ClapTrap1("Zebi");
-	ClapTrap ClapTrap2;
+	std::cout << "\n===CONSTRUCTOR DEFAULT CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap1;
+	ClapTrap1.attack("model_1");
+	ClapTrap1.takeDamage(200);
+	ClapTrap1.beRepaired(1);
 
-	ClapTrap1.attack("robot");
-	ClapTrap1.takeDamage(10);
-	ClapTrap1.beRepaired(10);
-	ClapTrap1.attack("robot");
+	std::cout << "\n===CONSTRUCTOR ASSIGNMENT CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap2("ClapTrap_Assignment");
+	ClapTrap2.attack("model_2");
 	ClapTrap2.takeDamage(9);
-	ClapTrap2.beRepaired(11);
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
-	ClapTrap2.attack("other robot");
+	ClapTrap2.beRepaired(10);
+
+	std::cout << "\n===CONSTRUCTOR COPY CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap3(ClapTrap1);
+	ClapTrap3.attack("model_3");
+	ClapTrap3.takeDamage(9);
+	ClapTrap3.beRepaired(10);
+
+	std::cout << "\n===CONSTRUCTOR COPY CLAPTRAP===\n" << std::endl;
+	ClapTrap ClapTrap4 = ClapTrap1;
+	ClapTrap4.attack("model_4");
+	ClapTrap4.beRepaired(10);
+
+	std::cout << "\n===DESTRUCTORS CLAPTRAP===\n" << std::endl;
 }
