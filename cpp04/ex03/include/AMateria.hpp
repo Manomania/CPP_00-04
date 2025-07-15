87c121e73b438aaae6e49d6349d03f304c88a4ad
maximart <maxime.martinez96@hotmail.fr>
2025-07-15 19:38:59 +0200

    feat(mandatory): add the necessary files submit by the subject

---
+++ cpp04/ex03/include/AMateria.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria {
+protected:
+public:
+	AMateria();
+	AMateria(std::string const & type);
+	virtual ~AMateria();
+	std::string const & getType() const;
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+};
---
+++ cpp04/ex03/src/main.cpp
@@ -0,0 +1,8 @@
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+
+int main() {
+	std::cout << "Coucou je compile." << std::endl;
+	return 0;
+}
