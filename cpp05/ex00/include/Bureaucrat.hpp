5fb1af66add3297d02671abe3a71b51d6d689322
maximart <maxime.martinez96@hotmail.fr>
2025-07-28 12:52:12 +0200

    feat(mandatory): add the necessary methods and private attributes

--- cpp05/ex00/include/Bureaucrat.hpp
+++ cpp05/ex00/include/Bureaucrat.hpp
@@ -3,8 +3,22 @@
 #include <iostream>
 
 class Bureaucrat {
-	private:
-	public:
-		Bureaucrat();
-		~Bureaucrat();
+private:
+	const std::string	_name;
+	size_t				_grade;
+public:
+	Bureaucrat(const std::string& name, size_t grade);
+	Bureaucrat(const Bureaucrat& copy);
+	Bureaucrat& operator=(const Bureaucrat& copy);
+	class GradeTooHighException: public std::exception{
+
+	};
+	class GradeTooLowException: public std::exception {
+
+	};
+	~Bureaucrat();
+	const std::string& getName() const;
+	size_t getGrade() const;
 };
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat& bc);
--- cpp05/ex00/src/Bureaucrat.cpp
+++ cpp05/ex00/src/Bureaucrat.cpp
@@ -1,7 +1,33 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() {
+Bureaucrat::Bureaucrat(const std::string& name, size_t grade): _name(name), _grade(grade) {
+
 }
 
 Bureaucrat::~Bureaucrat() {
+
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& copy): _name(copy._name), _grade(copy._grade) {
+
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& copy) {
+	if (this == &copy)
+		return (*this);
+	this->_grade = copy._grade;
+	return (*this);
+}
+
+const std::string& Bureaucrat::getName() const {
+	return (this->_name);
+}
+
+size_t Bureaucrat::getGrade() const {
+	return (this->_grade);
+}
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat& bc) {
+	os << bc.getName() << ", bureaucrat grade " << bc.getGrade() << std::endl;
+	return (os);
 }
