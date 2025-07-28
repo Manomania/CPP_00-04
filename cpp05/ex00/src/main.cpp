91d03b5e43a9711955ca02eadad7d52640127138
maximart <maxime.martinez96@hotmail.fr>
2025-07-28 11:37:58 +0200

    feat(mandatory): add the necessary files

---
+++ cpp05/ex00/src/main.cpp
@@ -0,0 +1,9 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+	std::cout << "Hello from ex00 project!" << std::endl;
+	Bureaucrat Bureaucrat_instance;
+	std::cout << "Using Bureaucrat" << std::endl;
+
+	return (0);
+}
