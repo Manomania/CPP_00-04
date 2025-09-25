c30326084f504cc917eb062b3bf62070aeab4dad
maximart <maxime.martinez96@hotmail.fr>
2025-09-25 12:31:08 +0200

    chore: removed the unnecessary files from the project

--- cpp05/ex02/include/PresidentialPardonForm.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "AForm.hpp"
-
-class PresidentialPardonForm: public AForm {
-public:
-	PresidentialPardonForm(const std::string& target);
-	virtual ~PresidentialPardonForm();
-	virtual void execute(Bureaucrat const& executor) const;
-};
